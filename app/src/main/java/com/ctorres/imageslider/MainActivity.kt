a498811d6421afcd30e5b948500322c2e3ed637d
ctorres <ctorres.developer@gmail.com>
2023-05-10 12:35:31 -0600

    First commit: Project created, Compose libraries added
    Signed-off-by: ctorres <ctorres.developer@gmail.com>

---
+++ app/src/main/java/com/ctorres/imageslider/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ctorres.imageslider
+
+import android.app.Activity
+import android.os.Bundle
+import android.os.PersistableBundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+}
