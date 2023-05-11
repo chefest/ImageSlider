b6ba979939ddbd0c60c0686b01ad3800f3345da2
ctorres <ctorres.developer@gmail.com>
2023-05-10 21:30:11 -0600

    Basic Image Slider component added.
    ImageSliderComponent composable with the basic image slider created.
    Sample of the implementation on MainActivity added.
    
    Signed-off-by: ctorres <ctorres.developer@gmail.com>

--- app/src/main/java/com/ctorres/imageslider/MainActivity.kt
+++ app/src/main/java/com/ctorres/imageslider/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.ctorres.imageslider
 
-import android.app.Activity
 import android.os.Bundle
-import android.os.PersistableBundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import com.ctorres.imageslider.components.ImageSliderComponent
 
-class MainActivity : Activity() {
+class MainActivity : ComponentActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                ImageSliderComponent(
+                    images = arrayOf(
+                        R.drawable.splash_drawio,
+                        R.drawable.splash_drawiob,
+                        R.drawable.splash_drawioc
+                    )
+                )
+            }
+        }
     }
 }
